93666ea97c207e6048b13334315d6d560145f20a
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-04-09 10:26:05 +0300

    add secondActivity (menu) use TablaLayout and change xml first activityMain

--- app/src/main/java/com/rorono/smarthouse/MainActivity.kt
+++ app/src/main/java/com/rorono/smarthouse/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.rorono.smarthouse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.rorono.smarthouse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnStart.setOnClickListener{
+            if (binding.etInputName.text.toString().isEmpty()) {
+                //Toast.makeText(this,"Please enter your name",Toast.LENGTH_LONG).show()
+                Snackbar.make(binding.linear, "Please enter your name", Snackbar.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(this, Menu::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        }
     }
-}
