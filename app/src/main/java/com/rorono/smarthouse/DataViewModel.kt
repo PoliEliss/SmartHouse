f5c793c7da0dfd8b27dd47dc8096bf4b93c7230c
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-04-10 13:52:47 +0300

    add viewModel

---
+++ app/src/main/java/com/rorono/smarthouse/DataViewModel.kt
@@ -0,0 +1,14 @@
+package com.rorono.smarthouse
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.lang.StringBuilder
+
+open class DataViewModel : ViewModel() {
+    val message: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val message2: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+}
